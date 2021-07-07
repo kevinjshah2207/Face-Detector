10cb7138c1932f9366ccb17e5f584a5dbb80de24
Kevin Shah <kevinjshah2207@gmail.com>
2021-07-07 16:09:02 -0700

    Added webcam functionality for face detection in real time

--- face_detector.py
+++ face_detector.py
@@ -1,33 +1,51 @@
 #Import the cv2 library
 import cv2
 
+#Import the randrange function for different colors of rectangles 
+from random import randrange
+
 # Load some pre-trained data on face frontals from opencv (haar cascade algorithm)
 
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 #Choose an image to detect the faces in
 # img = cv2.imread('/Users/kevinjshah2207/Desktop/Projects/Face_Detector/image1.jpg')
-img = cv2.imread('image1.jpg')
+# img = cv2.imread('image3.png')
 
-#Code to check of image is read properly
+#To capture video from webcam
+webcam = cv2.VideoCapture(0)
 
-if img is None:
-    print("Image is empty")
+#Iterate forever over frames
+while True:
+    ##Read the current frame
+    successful_frame, frame = webcam.read()
 
-#Convert image to grayscale
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-#Detect faces
-face_coordinates = trained_face_data.detectMultiScale(img_gray)
-print(face_coordinates)
-#Draw rectangles around faces
-(x,y,w,h) = face_coordinates[0]
-cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-#Display the image
-cv2.imshow('Face Detector Example', img)
+    #Code to check of image is read properly
 
-# Key to keep image displayed so that we can see it until we press any button
-cv2.waitKey()
+    # if img is None:
+    #     print("Image is empty")
+
+    #Convert image to grayscale
+    img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    #Detect faces
+    face_coordinates = trained_face_data.detectMultiScale(img_gray)
+
+    # print(face_coordinates)
+    #Draw rectangles around faces
+    for i in range(len(face_coordinates)):
+        (x,y,w,h) = face_coordinates[i]
+        # cv2.rectangle(frame, (x, y), (x+w, y+h), (randrange(256), randrange(256), randrange(256)), 5)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 5)
+    
+    #Display the image
+    cv2.imshow('Face Detector Example', frame)
+
+    # Key to keep image displayed so that we can see it until we press any button
+    cv2.waitKey(1)
+
+
 
 print("Code Completed")
